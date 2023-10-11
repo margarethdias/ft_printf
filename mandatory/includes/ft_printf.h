6f7465a28b656e8baadb5d4a69fab80b01604786
Margareth Dias <mdias@c3r12p4.42sp.org.br>
2023-10-11 20:54:32 -0300

    organized

---
+++ mandatory/includes/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias <mdias@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 19:50:29 by mdias             #+#    #+#             */
+/*   Updated: 2023/10/11 20:38:09 by mdias            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printchar(char c);
+int	ft_printstr(char *str);
+int	ft_printnbr(int nbr);
+
+#endif
